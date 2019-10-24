7bb15fe1d7f986d6b2e10f0196d7a335e180042d
RonakDelvadiya <34827211+RonakDelvadiya@users.noreply.github.com>
2019-10-24 12:42:31 +0530

    Update views.py
    remove comments

--- account/views.py
+++ account/views.py
@@ -5,13 +5,11 @@ from django.contrib.auth import authenticate, login, logout
 import re
 
 
-# Create your views here.
-
-
 def home(request):
     user = request.user.username
     return render(request, 'home.html',{'name':user})
-    
+
+
 def logout_views(request):
     logout(request)
     return render(request,'login.html',{}) 
@@ -19,7 +17,6 @@ def logout_views(request):
 
 def register(request):
     if request.method == 'POST':
-        print "in post"
         user_details=request.POST
         rpwd = user_details.get('rpassword')
         pwd = user_details.get('password')
@@ -29,44 +26,31 @@ def register(request):
         uname = user_details.get('username')
         queryset = User.objects.all().filter(username=uname)
         pass1 = pwd
-        #print queryset
         
         if queryset:
-            #print "if"
             return render(request,'registration.html',{'usererror': 'username already exist'})
         else:
-            #print "else"
             if re.match(r"^(?=.*\d).{6,12}$", pass1):
-                print "match"
                 if (rpwd == pwd):
                     user=User.objects.create_user(uname,email,pwd)
                     user.save()
-                #return render(request,'login.html',{})
                     return HttpResponseRedirect('/account/login_views/')
                 else:
                     return render(request,'registration.html',{'error': 'Your password and conform password both are not match'})
             else:
-                print "not match"
                 return render(request,'registration.html',{'passerror': 'Your  password must be between 6 and 12 digits long and include at least one numeric digit.'})
     return render(request,'registration.html')
    
 
 def login_views(request):
-    print "in login"
-    print request.method
     if request.method == 'POST':
-
-        print "from log in post"
         form_data=request.POST
         uname=form_data.get('username')
         pwd=form_data.get('password')
-        # queryset = User.objects.all().filter(username=uname).filter (password=pwd)
-        # print queryset
         user = authenticate(username=uname, password=pwd)
         if user is not None:
             login(request, user)
             return HttpResponseRedirect('/account/home/')
         else:
             return render(request, 'login.html',{'error':'your username and password are invalid'})
-    print "from log out of post"
     return render(request,'login.html',{})
